26c1d27073ce8acf098c6fa51e7b976db5247c16
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-06 20:29:57 -0300

    Added new request
    Added a request sending a file.

--- myApp.js
+++ myApp.js
@@ -2,6 +2,13 @@ let express = require('express');
 let app = express();
 console.log( "Hello World");
 
+/* app.get( "/", function( req, res){
+   res.send( "Hello Express");
+});*/
+  
+app.get( "/", ( request, response) =>{
+   response.sendFile(__dirname + "/views/index.html" );
+});
 
 
 
