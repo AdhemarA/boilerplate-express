b49911ec5f3d0ac9214fd4a2505f986dad38b2f4
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-06 23:18:46 -0300

    more corrections
    remanufacturing code

--- myApp.js
+++ myApp.js
@@ -13,22 +13,11 @@ app.get( "/", ( request, response) =>{
 
 //app.use( express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
-
-let jsonMessag = { "message" : "Hello json" };
-/* app.get( "/json", ( request, response) =>{
-   response.json(jsonMessag );
-});*/
-
+let jsonMessag;
+process.env.MESSAGE_STYLE === "uppercase" ? jsonMessag= { "message" : "HELLO JSON" }: jsonMessag = { "message" : "Hello json" };
 app.get( "/json", ( request, response) =>{
-   if(process.env.MESSAGE_STYLE === "uppercase"){
-      jsonMessag = { "message" : "HELLO JSON" };
-      console.log( "step 1");
-   }else{
-      jsonMessag = { "message": "Hello json"};
-   console.log( "step 2");
-   };
+   response.json(jsonMessag );
 });
-
 
 
 
