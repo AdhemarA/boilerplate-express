fb628430e9839b005d99d117ed0d5b2b71f3af74
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-07 00:45:13 -0300

    more environment
    issuing environment

--- myApp.js
+++ myApp.js
@@ -14,7 +14,8 @@ app.get( "/", ( request, response) =>{
 //app.use( express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 let jsonMessag = {};
-console.log( process.env.MESSAGE_STYLE);
+let prueba= process.env.MESSAGE_STYLE; 
+console.log( process.env);
 process.env.MESSAGE_STYLE === "uppercase" ? jsonMessag= { "message" : "HELLO JSON" }: jsonMessag = { "message" : "Hello json" };
 app.get( "/json", ( request, response) =>{
    response.json(jsonMessag );
