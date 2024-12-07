a075b2f211fb7651f3e6843a4d6dfd15903765dc
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-06 23:27:54 -0300

    nuevo cambio
    Buscar error

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log( "Hello World");
@@ -14,6 +14,7 @@ app.get( "/", ( request, response) =>{
 //app.use( express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 let jsonMessag;
+console.log( process.env.MESSAGE_STYLE);
 process.env.MESSAGE_STYLE === "uppercase" ? jsonMessag= { "message" : "HELLO JSON" }: jsonMessag = { "message" : "Hello json" };
 app.get( "/json", ( request, response) =>{
    response.json(jsonMessag );
